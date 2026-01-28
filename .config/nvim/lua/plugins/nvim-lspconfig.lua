29b11406d2793b4297115a806f77a6a73d5e8571
nbfhscl <nbfhscl@letmeout.win>
2026-01-28 13:18:37 +0000

    feature: 1. lsp config markdown lint execude and pyright project root.\n 2. tmux config mouse on to scroll.

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,19 +1,39 @@
-return
-{
-  {
-    'neovim/nvim-lspconfig',
-    opts = {
-      servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              globals = { 'vim' },
-            }
-          }
-        }
-      }
-    },
-  },
-  --, before = {'williamfzc/nvim-diagnostic' }},
-  -- { 'williamfzc/nvim-diagnostic', requires = 'neovim/nvim-lspconfig'},
+return {
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				lua_ls = {
+					settings = {
+						Lua = {
+							globals = { "vim" },
+						},
+					},
+				},
+				marksman = {
+					settings = {
+						marksman = {
+							-- 全局禁用某些规则
+							disabledRules = { "MD013", "MD024", "MD031" }, -- 行长、重复标题等
+							-- 或启用自定义配置文件
+							configPath = vim.fn.expand("~/.config/markdownlint.json"),
+						},
+					},
+				},
+				pyright = {
+					settings = {
+						python = {
+							analysis = {
+								-- Add current project root to module search path
+								extraPaths = { vim.fn.getcwd() },
+								autoSearchPaths = true,
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	--, before = {'williamfzc/nvim-diagnostic' }},
+	-- { 'williamfzc/nvim-diagnostic', requires = 'neovim/nvim-lspconfig'},
 }
