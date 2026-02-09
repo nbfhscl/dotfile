75c5eb9365a1e34baa39435e7ad9c0b5099593a8
nbfhscl <nbfhscl@letmeout.win>
2026-02-10 00:57:51 +0800

    feature: which-key.lua <c-w><space> to trigger hybra mode.

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"folke/which-key.nvim",
+		-- keys = {
+		-- 	{
+		-- 		"<leader>?",
+		-- 		function()
+		-- 			require("which-key").show({ global = false })
+		-- 		end,
+		-- 		desc = "Buffer Keymaps (which-key)",
+		-- 	},
+		-- 	{
+		-- 		"<c-w><space>",
+		-- 		function()
+		-- 			require("which-key").show({ keys = "<c-w>", loop = true })
+		-- 		end,
+		-- 		desc = "Window Hydra Mode (which-key)",
+		-- 	},
+		-- },
+	},
+}
