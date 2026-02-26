9f40026ad0627663e5c2df56202696846550fc25
nbfhscl <nbfhscl@letmeout.win>
2026-02-26 13:42:16 +0800

    feature: vim，tmux依次控制修改终端标题，用于远端基于终端标题做自动化控制，如自动切换输入法。

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,7 @@ vim.g.lazyvim_python_lsp = "basedpyright"
 -- In case you don't want to use `:LazyExtras`,
 -- then you need to set the option below.
 -- vim.g.lazyvim_picker = "fzf"
+-- Enable terminal/window title updates (needed for the Vim[INSERT]/Vim[NORMAL] marker)
+vim.opt.title = true
+vim.opt.titleold = "" -- optional: title when exiting Neovim (only works if 'title' is on)
+-- vim.opt.titlelen = 0 -- optional: 0 means do not shorten the title
